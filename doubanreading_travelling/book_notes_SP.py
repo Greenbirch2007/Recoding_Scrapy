e5360173aa8db95d75c4c19cdb75c2b59350b198
Greenbirch2007 <291109028@qq.com>
2018-10-16 14:46:05 +0800

    数据完毕，回头写接口

--- doubanreading_travelling/book_notes_SP.py
+++ doubanreading_travelling/book_notes_SP.py
@@ -1,8 +1,11 @@
+import datetime
+from multiprocessing.pool import Pool
+
 import pymysql
 import requests
 from lxml import etree
 from requests.exceptions import RequestException
-
+import re
 def call_page(url):
     try:
         response = requests.get(url)
@@ -17,17 +20,21 @@ def call_page(url):
 
 def parse_note(html):
 
+
     selector = etree.HTML(html)
 
     title = selector.xpath('//*[@id="wrapper"]/h1/span/text()')
     short_comment = selector.xpath('//*[@id="comments"]/ul/li[1]/div/p/span/text()')
     book_notes = selector.xpath('//*[@id="content"]/div/div[1]/div[3]/div[14]/div[2]/ul[2]/li[1]/div[2]/div[2]/div/div[1]/span/text()')
-    
-
-
-
-
-
+    # 切割后再拼接
+    ts1 = "".join(short_comment)
+    ts_list1 = []
+    ts_list1.append(ts1)
+    ts2 = "".join(book_notes)
+    ts_list2 = []
+    ts_list2.append(ts2)
+    for i1,i2,i3 in zip(title,ts_list1,ts_list2):
+        return  [(i1,i2,i3)]
 
 
 
@@ -38,7 +45,7 @@ def Python_sel_Mysql():
     cur = connection.cursor()
     #sql 语句
     for i in range(1,1001):
-        sql = 'select coding from js_infos where id = %s ' % i
+        sql = 'select book_link from DBbook_trip where id = %s ' % i
         # #执行sql语句
         cur.execute(sql)
         # #获取所有记录列表
@@ -58,10 +65,17 @@ def insertDB(content):
     connection.close()
     print('向MySQL中添加数据成功！')
 
-url = 'https://book.douban.com/subject/5414391/'
-html = call_page(url)
-content =parse_note(html)
-insertDB(content)
+
+
+
+
+if __name__ == '__main__':
+    pool = Pool(4)
+    for url_str in Python_sel_Mysql():
+        html = call_page(url_str)
+        content = parse_note(html)
+        insertDB(content)
+        print(datetime.datetime.now())
 
 # create table book_contents(
 # id int not null primary key auto_increment,
@@ -69,4 +83,4 @@ insertDB(content)
 # short_comment text,
 # book_notes text
 # )engine=InnoDB  charset=utf8;
-#
+# #
